17d9bb6cc36ece09d3efbd519e62109e30127d22
rafaellg8 <rafaellg8@correo.ugr.es>
2015-10-28 11:39:59 +0100

    Probando test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 var assert = require("assert"),
 	prueba = require(__dirname+"/../models/taskDao.js");
+	prueba2 = require(__dirname+"/../routes/tasklist.js");
 
 describe('init', function(){
 	// Testea que se haya cargado bien la librería
-
 	it('Cargando la base de datos', function(){
 		assert(prueba, "Cargado documentDB de Azure");
 	});
@@ -12,5 +12,11 @@ describe('init', function(){
 describe('addItem', function () {
     it('Añadimos una nueva nota de empresa', function(){
 		assert(prueba, "Creada la empresa correctamente");
+	});
+})
+
+describe('addItem', function () {
+	if('Añadiendo empresa en el acto',function(){
+		assert(prueba2, "Creando la empresa en tiempo real correctamente");
 	});
 })
